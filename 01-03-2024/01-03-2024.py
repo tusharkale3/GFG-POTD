5f32128c509100d052367956199e5f68f2b5318d
kaletushar3112 <kaletushar3112200@gmail.com>
2024-03-01 12:14:32 +0530

    01-03-2024

---
+++ 01-03-2024/01-03-2024.py
@@ -0,0 +1,49 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+# your task is to complete this function
+# function should return index to the any valid peak element
+class Solution:   
+    def peakElement(self, arr, n):
+        left, right = 0, n - 1
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if arr[mid] > arr[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+
+#{ 
+ # Driver Code Starts
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        index = Solution().peakElement(arr.copy(), n)
+        flag = False
+        if index<0 or index>=n:
+            flag = False
+        else:
+            if index == 0 and n==1:
+                flag = True
+            elif index==0 and arr[index]>=arr[index+1]:
+                flag = True
+            elif index==n-1 and arr[index]>=arr[index-1]:
+                flag = True
+            elif arr[index-1] <= arr[index] and arr[index] >= arr[index+1]:
+                flag = True
+            else:
+                flag = False
+
+        if flag:
+            print(1)
+        else:
+            print(0)
+# Contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
